f11e769cdc35d1a24a1bab2b05e4330de3c5d68e
Arif khan <ak4040651@gmail.com>
2024-12-17 20:38:16 +0530

    solved qustion

---
+++ even-odd/even.js
@@ -0,0 +1,8 @@
+// Write a program that takes an integer input and prints whether it's even or odd.
+function isEven(num){
+    if(num % 2 === 0){
+        return`${num} is even`;
+    }else{
+        return`${num} is odd`;
+    }
+}
